671956a6b46747e17b9a80aa48e39f6b3e798573
Edwinngera <edwin@OCA273.localdomain>
2022-07-01 15:21:42 +0300

    microservices

---
+++ run_kubernetes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="edwinngera/microservices"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run ml-microservice-kubernetes --image=docker.io/$dockerpath --port=80
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward ml-microservice-kubernetes 8000:80
---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="edwinngera/microservices"
+
+# Step 2:  
+# Authenticate & tag
+docker login
+docker tag microproject ${dockerpath}
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker push ${dockerpath}
